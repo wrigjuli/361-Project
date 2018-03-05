cb177b2f0af7ad0264df473379f3cde4167f2445
James M <james.p.mitchell5@gmail.com>
2018-03-05 00:02:40 -0500

    im still new to git

--- index.js
+++ index.js
@@ -1,31 +1,66 @@
+/*
+Group 29
+CS361
+March 5, 2018
+Part of Assignment 6 implementation Requirements
+
+This will be our main file. Eventually we will type run forever start index.js and we will be able to acces that website
+*/
+
 //This requires the datafile which containes the array of 20 charities. 
 var cArray = require('./data.js')
 var cdb = cArray.cdb;
 
-// This is James messing around with testing functionality
-console.log(typeof(cdb[0]));
-console.log(cdb[0]["ein"]);
 
 /*
-The database is an ARRAY of 20 charities imported from the data.js file. This array is called 'cdb'
-Each member of the array is organized as a JSON format. 
-[
-    {key1:charity1Value1, key2:charity1Value2 ... key-n:charity1Value-n},
-    {key1:charity2Value1, key2:charity2Value2 ... key-n:charity2Value-n},
-    .
-    .
-    .
-    {key-z:charity-z-Value1, key2:charity-z-Value2 ... key-n:charity-z-Value-n}
-]
-
-To pull a charity object call cdb[i]
-To go within that object to reach a property call cdb[i]["property"]
-
-Example: Set a new variable equal to the mission of the 5th Charity. Uncomment below to see it work. 
+Includes the search class accessible under the namespace search
+Type: Class
+Parameter: (searchphrase:string , database:jsonArray)
 */
-//var mission = cdb[4]["mission"]; console.log(mission);
-/*
+var search = require('./search.js')
 
-*/
+//Showing that the search class works
+let testSearch = new search("children", cdb);
+testSearch.testMethod();
 
 
+
+//various required modules to host a website with node.js
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
+
+
+//This is the test homepage. You can see it is passed a JSON which will eventually be the results JSON
+
+app.get('/', function (req, res, next) {
+    var context = {title:"Search Result",body:JSON.stringify(testSearch.getCharity(3))};
+    res.render('home',context);
+    //The home handlebars is the one I just did a little bit to. 
+
+});
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+  });
+  
+  app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+  });
+  
+  app.listen(app.get('port'), function(){
+    console.log('Express started on (# wrong->?) flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+  });
